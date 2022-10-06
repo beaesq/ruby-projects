56e9ae5b18287ddaf0ee4ec901b3d19fceca8648
Bwa <40144023+beaesq@users.noreply.github.com>
2022-10-06 23:30:16 +0800

    Add input getter, win checker. Start game loop

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -1,11 +1,15 @@
 class Game
   def initialize
-    @guessed_letters = {}
-    @guess_counter = 0
+    @guessed_letters = []
+    @wrong_guesses = 0
     @correct_word = random_word
   end
 
-  def self.random_word
+  attr_accessor :guessed_letters, :wrong_guesses
+  attr_reader :correct_word
+
+  private
+  def random_word
     word = ''
     until word.length <= 12 && word.length >= 5 do 
       word_file = File.open('google-10000-english-no-swears.txt', 'r')
@@ -31,3 +35,41 @@ def draw_hangman(num)
   puts ' ║'
   puts '═╩════════'
 end
+
+def input_letter
+  print 'Enter a letter: '
+  letter = gets.chomp
+  raise 'Please enter a letter.' unless letter.match?(/[a-zA-Z]/)
+  letter.downcase
+rescue StandardError => e
+  retry
+end
+
+def draw_screen(current_game)
+  clear
+  draw_hangman(current_game.wrong_guesses)
+end
+
+def clear
+  if Gem.win_platform?
+    system 'cls'
+  else
+    system 'clear'
+  end
+end
+
+def game_over?(current_game)
+  current_game.wrong_guesses >= 6 || is_word_guessed?(current_game)
+end
+
+def is_word_guessed?(current_game)
+  word_array = current_game.correct_word.split(//).uniq
+  (current_game.guessed_letters & word_array).size == word_array.size
+end
+
+current_game = Game.new
+until game_over?(current_game) do 
+  draw_screen(current_game)
+end
+# %w[a b c d e f g h i j k l m n]
+# %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
