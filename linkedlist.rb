7a392907c4361c29113a5d3a4aceabc30876ca15
Bwa <40144023+beaesq@users.noreply.github.com>
2022-10-15 21:07:51 +0800

    Add contains, find, to_s

--- linkedlist.rb
+++ linkedlist.rb
@@ -55,6 +55,36 @@ class LinkedList
     @tail.next_node = nil
     old_tail
   end
+
+  def contains?(value)
+    current_node = @head
+    until current_node.nil?
+      return true if current_node.value == value
+      current_node = current_node.next_node
+    end
+    false
+  end
+
+  def find(value)
+    current_node = @head
+    index = 0
+    until current_node.nil?
+      return index if current_node.value == value
+      index += 1
+      current_node = current_node.next_node
+    end
+    nil
+  end
+
+  def to_s
+    current_node = @head
+    str = ''
+    until current_node.nil?
+      str << "( #{current_node.value} ) -> "
+      current_node = current_node.next_node
+    end
+    str << 'nil'
+  end
 end
 
 class Node
@@ -68,14 +98,18 @@ end
 
 p list = LinkedList.new
 # p "head: #{list.head} tail: #{list.tail}"
+p list.to_s
 list.append('dog')
+p list.to_s
 list.append('cat')
+p list.to_s
 list.prepend('rock')
-pp list
-p "0: #{list.at(0).value}"
-p "1: #{list.at(1).value}"
-p "2: #{list.at(2).value}"
-p list.size
-p list.pop
-pp list
-p list.size
+p list.to_s
+# pp list
+# p "0: #{list.at(0).value}"
+# p "1: #{list.at(1).value}"
+# p "2: #{list.at(2).value}"
+# p list.find('rock')
+# p list.find('dog')
+# p list.find('cat')
+# p list.find('chicken')
