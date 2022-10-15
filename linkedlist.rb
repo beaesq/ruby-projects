c6274298f1e4d147c528e85efba780a77e11ea96
Bwa <40144023+beaesq@users.noreply.github.com>
2022-10-15 20:00:19 +0800

    Add append and prepend

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,12 +1,45 @@
 class LinkedList
+  def initialize
+    @head = nil
+    @tail = nil
+  end
 
+  attr_accessor :name
+
+  def append(value)
+    new_node = Node.new(value, nil)
+    if @head.nil?
+      @head = new_node
+    else
+      @tail.next_node = new_node
+    end
+    @tail = new_node
+  end
+
+  def prepend(value)
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      new_node.next_node = @head
+      @head = new_node
+    end
+  end
 end
 
 class Node
-  def initalize(value = nil, next_node = nil)
+  def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
   end
 
-  attr_accessor :value
+  attr_accessor :value, :next_node
 end
+
+p list = LinkedList.new
+list.append('dog')
+list.append('cat')
+pp list
+list.prepend('rock')
+pp list
