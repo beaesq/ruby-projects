586749571d709d32e72da7f478fa766c93e2e70b
Bwa <40144023+beaesq@users.noreply.github.com>
2022-10-21 20:24:57 +0800

    Add node class, some functions from binned version

--- knight.rb
+++ knight.rb
@@ -1,80 +1,80 @@
 # frozen_string_literal: true
 
-# squares on the board
+# square nodes
 class Square
-  def initialize(coordinates)
+  def initialize(coordinates, parent = nil)
     @coordinates = coordinates
     @x = coordinates[0]
     @y = coordinates[1]
-    @north = nil
-    @west = nil
-    @south = nil
-    @east = nil
+    @children = []
+    @parent = parent
   end
 
-  attr_accessor :coordinates, :x, :y, :north, :west, :south, :east
-
-  def connect_north_south_square!(south_square)
-    self.south = south_square
-    south_square.north = self unless south_square.nil?
-  end
-
-  def connect_east_west_square!(west_square)
-    self.west = west_square
-    west_square.east = self unless west_square.nil?
-  end
+  attr_accessor :coordinates, :x, :y, :children, :parent
 end
 
+# board
 class Board
   def initialize
     @board_array = []
     make_board
-    connect_north_south
-    connect_east_west
-  end
-
-  attr_reader :board_array, :root
-
-  private
-
-  def connect_north_south
-    north_sq = nil
-    for x in 0..7
-      for y in 0..7
-        south_sq = y.zero? ? nil : north_sq
-        north_sq = @board_array[x][y]
-        north_sq.connect_north_south_square!(south_sq)
-      end
-    end
-  end
-
-  def connect_east_west
-    east_sq = nil
-    for y in 0..7
-      for x in 0..7
-        west_sq = x.zero? ? nil : east_sq
-        east_sq = @board_array[x][y]
-        east_sq.connect_east_west_square!(west_sq)
-      end
-    end
   end
 
   def make_board
     line_array = []
-     for x in 0..7
-      for y in 0..7
+    8.times do |x|
+      8.times do |y|
         new_sq = Square.new([x, y])
         line_array << new_sq
-        @root = new_sq if [0, 0].eql?([x, y])
+        # @root = new_sq if [0, 0].eql?([x, y])
       end
       @board_array << line_array
       line_array = []
     end
   end
+
+  def find(coordinates)
+    return nil if coordinates.nil?
+
+    x = coordinates[0]
+    y = coordinates[1]
+    @board_array[x][y]
+  end
+
+  attr_reader :board_array
+
+  def knight_moves(start_coordinates, end_coordinates)
+    @queue = []
+    @unvisited_squares = list_all_squares
+    @queue.push(start_coordinates) if @queue.empty?
+    build_knight_tree(start_coordinates, end_coordinates)
+  end
+
+  def build_knight_tree(start_coordinates, end_coordinates)
+
+  end
+
+  def make_children(current_square)
+    moves = [[-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1]]
+    moves.map! { |move| move.zip(current_square.coordinates).map { |a, b| a + b } }
+    moves.keep_if { |x, y| x <= 7 && x >= 0 && y <= 7 && y >= 0 }
+    current_square.children = moves.map { |move| find(move) }
+  end
+
+  def enqueue_children(current_square)
+    current_square.children.each { |child| @queue.push(child.coordinates) }
+  end
+
+  def list_all_squares
+    arr = []
+    8.times do |x|
+      8.times do |y|
+        arr << [x, y]
+      end
+    end
+    arr
+  end
 end
 
 board = Board.new
-pp board.board_array[0][0].west
-# pp board.board_array[1][1].west
-# pp board.board_array[4][3]
-# pp board.board_array[7][7]
+p board.knight_moves([0,0], [2,2])
